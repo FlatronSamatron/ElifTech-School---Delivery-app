dc66e4c530f7ba07da03ad7e9c6875b03f6b8b4e
Vitalii <flatronsamatron@gmail.com>
2023-05-27 12:25:36 +0300

    add new features

--- client/src/redux/actions/shops.js
+++ client/src/redux/actions/shops.js
@@ -5,7 +5,8 @@ export const fetchShops = () => {
     return async (dispatch) => {
         try {
             dispatch({type: shopTypes.FETCH_SHOPS})
-            const res = await axios.get('/api/shops')
+            // const res = await axios.get('/api/shops')
+            const res = await axios.get('https://flatronsamatron.onrender.com/api/shops')
             dispatch({type: shopTypes.FETCH_SHOPS_SUCCESS, payload: res.data})
         } catch (error){
             dispatch({type: shopTypes.FETCH_SHOPS_ERROR, payload: error})
